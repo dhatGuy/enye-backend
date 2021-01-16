9b3f3f857d5f70f7ac9c2986532af1c40ecefde8
dhatGuy <odunsiolakunbi@gmail.com>
2021-01-16 14:05:45 +0100

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const rates = require("./routes/rates");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
